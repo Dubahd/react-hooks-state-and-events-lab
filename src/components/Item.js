2285cf5f047d25f674f65e2fdad38cb9870dc91f
Duba Halakhe <dubahdida@gmail.com>
2025-10-28 14:20:22 +0300

    lab complete

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [inCart, setInCart] = useState(false);
+
+  function handleAdded() {
+    setInCart((prev) => !prev);
+  }
+
+  const color = inCart ? "red" : "yellow";
+  const addition = inCart ? "Remove From Cart" : "Add to Cart";
+  const lineThrough = inCart ? "line-through" : "";
+
   return (
-    <li className="">
+    <li className={inCart ? "in-cart" : ""}>
       <span>{name}</span>
-      <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <span
+        className="category"
+        style={{ textDecoration: lineThrough }}
+      >
+        {category}
+      </span>
+      <button
+        className="add"
+        onClick={handleAdded}
+        style={{ backgroundColor: color }}
+      >
+        {addition}
+      </button>
     </li>
   );
 }
